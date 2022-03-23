d362a1c47a83a56ce44f60422f1b4dd1a7667dba
michael moura <michael.moura@gtplan.com.br>
2022-03-23 16:58:47 -0300

    second challenger completed

---
+++ challenges/JavaScript Fundamentals – Part 1/script.js
@@ -0,0 +1,14 @@
+//first chalenger
+let country = "Brazil";
+let continent = "South America";
+let population = 212600000.00;
+
+//second chalenger
+let isIsland = false;
+let language;
+
+console.log(isIsland);
+console.log(population);
+console.log(country);
+console.log(language);
+
--- challenges/JavaScript Fundamentals – Part 1/values and variables/script.js
+++
@@ -1,8 +0,0 @@
-//first chalenger
-const country = "Brazil";
-const continent = "South America";
-const population = 212600000.00;
-
-console.log(country);
-console.log(continent);
-console.log(population);
