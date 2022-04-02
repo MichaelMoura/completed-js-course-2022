90d590f50d6b4c6a7c2fcd214becfe83c29cd4f3
michael moura <michael.moura@gtplan.com.br>
2022-04-02 12:59:27 -0300

    JS Fundamentals Part I: tenth assignment completed

--- Assignments/JavaScript Fundamentals – Part 1/script.js
+++ Assignments/JavaScript Fundamentals – Part 1/script.js
@@ -43,13 +43,13 @@ console.log(5 + 6 + '4' + 9 - 4 - 2);
 //seventh assignment - equality operators
 // const numNeighbours = Number(prompt("How many neighbour countries does your country have"));
 
-if (numNeighbours === 1) {
-    console.log("Only 1 border");
-} else if (numNeighbours > 1) {
-    console.log("more then 1 border");
-} else {
-    console.log("No borders");
-}
+// if (numNeighbours === 1) {
+//     console.log("Only 1 border");
+// } else if (numNeighbours > 1) {
+//     console.log("more then 1 border");
+// } else {
+//     console.log("No borders");
+// }
 
 //eighth assignment - Logical Operators
 
@@ -58,3 +58,36 @@ if (population < 50000000 && language === "English") {
 } else {
     console.log("Brazil does not meet your criteria :(")
 }
+
+//nineth assignment - the switch statement
+
+const yourLanguage = prompt("What is your language?").trim().toLocaleLowerCase()
+
+switch (yourLanguage) {
+    case "chinese":
+    case "mandarim":
+        console.log("Most number of native speakers!")
+        break;
+    case "spanish":
+        console.log("2nd place in number of native speakers");
+        break;
+    case "english":
+        console.log("3rd place");
+        break;
+    case "hindu":
+        console.log("Number 4")
+        break
+    case "arabic":
+        console.log("5th most spoken language");
+        break;
+    default:
+        console.log("Great language too :D")
+
+}
+
+//tenth assignment - ternary operator
+
+
+population > 33000000
+    ? console.log("Brazil population is above average")
+    : console.log("Brazil popoulation is below average")
