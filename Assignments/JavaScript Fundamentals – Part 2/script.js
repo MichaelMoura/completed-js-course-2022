114a85e3ff8ea95789d64733028abb92a0f46e4c
michael moura <michael.moura@gtplan.com.br>
2022-04-02 20:12:46 -0300

    JS Fundamentals Part II: second  assignment completed

--- Assignments/JavaScript Fundamentals – Part 2/script.js
+++ Assignments/JavaScript Fundamentals – Part 2/script.js
@@ -8,3 +8,20 @@ describeContry("Brazil", 212000000, "Brasilia");
 describeContry("Portugal", 10310000, "Lisbon");
 describeContry("Germany", 83240000, "Berlin");
 
+//second assignment - function declarations vs expressions
+
+function percentageOfWord1(population) {
+    return (population / 7900000000) * 100;
+}
+
+console.log(percentageOfWord1(340034034));
+console.log(percentageOfWord1(200034034));
+console.log(percentageOfWord1(490034034));
+
+const percentageOfWord2 = function (population) {
+    return (population / 7900000000) * 100;
+}
+
+console.log(percentageOfWord1(230034034));
+console.log(percentageOfWord1(41034034));
+console.log(percentageOfWord1(830034034));
