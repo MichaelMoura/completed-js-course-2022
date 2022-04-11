9b94a668be39a3473eb0d74f533d00b09fdc517b
michael moura <michael.f.m2012@gmail.com>
2022-04-11 11:50:22 -0300

    functions: second challenge completed

--- challenges/5 - Functions/challenge2/script.js
+++ challenges/5 - Functions/challenge2/script.js
@@ -1,1 +1,14 @@
 'use strict';
+
+let isColored = false;
+
+(function () {
+    const header = document.querySelector('h1');
+
+    header.addEventListener("click", () => {
+        !isColored ? header.style.color = 'blue' : header.style.color = 'white';
+        !isColored ? isColored = true : isColored = false;
+    })
+})();
+
+[1, 2, 3].at()
