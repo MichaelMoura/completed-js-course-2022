076cc385df5f596227d0b238d8af775457453794
michael moura <michael.f.m2012@gmail.com>
2022-04-22 15:59:06 -0300

    OOP: second challenge completed

--- challenges/7 - OOP/challenge2/script.js
+++ challenges/7 - OOP/challenge2/script.js
@@ -1,1 +1,26 @@
 'use strict';
+
+class CarlCL {
+    constructor(make, speed) {
+        this.make = make;
+        this.speed = speed;
+    }
+
+    accelerate() {
+        this.speed += 10;
+        console.log(`${this.make} is going at ${this.speed}km/h`)
+    }
+
+    brake() {
+        this.speed -= 5;
+        console.log(`${this.make} brake at ${this.speed}km/h`)
+    }
+
+    get speedUs() {
+        return this.speed / 1.6;
+    }
+
+    set speedUs(speed) {
+        this.speed = speed * 1.6;
+    }
+}
